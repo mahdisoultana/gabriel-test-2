b23e74056fa7822aa862881fbf52f2456d697a3f
Mahdi-Soultana <soultana.mahdi24@gmail.com>
2022-10-08 18:38:21 +0100

    key index

--- src/components/layout/asides/asideLeft/accrodionLists/AccordionListAside.jsx
+++ src/components/layout/asides/asideLeft/accrodionLists/AccordionListAside.jsx
@@ -4,8 +4,8 @@ import Basics from "./basics/Basics";
 function AccordionListAside() {
   return [Basics, Basics, Basics, Basics, Basics, Basics].map(
     (List, index, array) => (
-      <section className={index == array.length - 1 && "pb-4"}>
-        <List key={index} />
+      <section key={index} className={index == array.length - 1 && "pb-4"}>
+        <List />
       </section>
     ),
   );
